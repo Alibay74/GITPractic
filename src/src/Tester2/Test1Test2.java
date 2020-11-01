f990c16548687c495599c33f0c70e9bff46c0328
Ali-apple <aitov.74@list.ru>
2020-11-01 12:31:46 -0500

    two classes added

---
+++ src/src/Tester2/Test1Test2.java
@@ -0,0 +1,7 @@
+package src.Tester2;
+
+public class Test1Test2 {
+public static void main(String[] args) {
+	
+}
+}
---
+++ src/src/Tester3/Tester2_Test3.java
@@ -0,0 +1,10 @@
+package src.Tester3;
+
+public class Tester2_Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
